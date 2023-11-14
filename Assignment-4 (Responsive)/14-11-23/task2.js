2be66cddf18cd07d86956f8ab7b84dec5566eae2
Umar Saeed <umar23lhr@gmail.com>
2023-11-14 09:26:44 +0500

    Task 2

---
+++ Assignment-4 (Responsive)/14-11-23/task2.js
@@ -0,0 +1,28 @@
+var day = 'sunday';
+switch (day) {
+    case 'monday':
+        console.log('do something')
+        break;
+    case 'tuesday':
+        console.log('do something')
+        break;
+    case 'wednesday':
+        console.log('do something')
+        break;
+    case 'thursday':
+        console.log('do something')
+        break;
+    case 'friday':
+        console.log('do something')
+        break;
+    case 'saturday':
+        console.log('do something')
+        break;
+    case 'sunday':
+        console.log('do something')
+        break;
+
+    default:
+        console.log('There is No such Day')
+        break;
+}
