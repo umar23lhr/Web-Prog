9a65ffe2d50a5a93fa8333039efcf2ada85b3da4
Umar Saeed <umar23lhr@gmail.com>
2023-11-14 10:24:01 +0500

    Task 3

---
+++ Assignment-4 (Responsive)/14-11-23/task3.js
@@ -0,0 +1,15 @@
+var i = 1;
+for(i=1 ; i<=8 ; i++)
+{
+    if (i==1)
+    {
+        console.log('Gold Medal');
+    }
+    else if(i ==2 ){
+        console.log('Silver Medal') ;
+    }
+    else if(i ==3){
+        console.log('Bronze Medal') ;}
+    else
+    console.log(i);
+}
