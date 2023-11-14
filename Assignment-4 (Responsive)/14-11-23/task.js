29d40ea734d590cac1c7e3ff2bec80ce177fdba1
Umar Saeed <umar23lhr@gmail.com>
2023-11-14 09:18:48 +0500

    Task 1

---
+++ Assignment-4 (Responsive)/14-11-23/task.js
@@ -0,0 +1,15 @@
+var age = 10;
+if(age>=65){
+    console.log('You Get your income from your pension' );
+}
+else if(age<65 && age>18){
+    console.log('each month you get a salary')
+}
+else if(age<18)
+{
+    console.log('you get an allowance')
+}
+else
+{
+    console.log('The value of age variable is not numerical')
+}
