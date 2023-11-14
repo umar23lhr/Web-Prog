195845576467940c9d9e35bdf0f9af72cde494d4
Umar Saeed <umar23lhr@gmail.com>
2023-11-14 10:52:55 +0500

    Changes

--- Assignment-4 (Responsive)/14-11-23/abc.js
+++ Assignment-4 (Responsive)/14-11-23/abc.js
@@ -1,64 +1,64 @@
-// var result = 40;
-// if (result=40)
-// {
-//     console.log("Congratulation you passed.");
-// }
-// else{
-//     console.log("Unfortunately you did not pass.")
-// }
-// // -------------------------------------------------------
-// var food = "cold";
-// if(food=="hot"){
-//     console.log('Too hot')
-// }
-// else if (food=="cold"){
-//     console.log('Too cold')
-// }
-// else{
-//     console.log('Just Right')
-// }
-// // ------------------------------------------------------------
-// var place = 'second';
-// switch (place) {
-//     case 'first':
-//         console.log('Gold')
-//         break;
-//     case 'second':
-//         console.log('Silver')
-//         break;
-//     case 'third':
-//         console.log('Bronze')
-//         break;
+var result = 40;
+if (result=40)
+{
+    console.log("Congratulation you passed.");
+}
+else{
+    console.log("Unfortunately you did not pass.")
+}
+// -------------------------------------------------------
+var food = "cold";
+if(food=="hot"){
+    console.log('Too hot')
+}
+else if (food=="cold"){
+    console.log('Too cold')
+}
+else{
+    console.log('Just Right')
+}
+// ------------------------------------------------------------
+var place = 'second';
+switch (place) {
+    case 'first':
+        console.log('Gold')
+        break;
+    case 'second':
+        console.log('Silver')
+        break;
+    case 'third':
+        console.log('Bronze')
+        break;
 
-//     default:
-//         console.log('No Medal')
-//         break;
-// }
-// // ---------------------------------------------------------- LOOP -------------------------------------------
-// var i=1;
-// for(i=1;i<4;i++){
-//     console.log(i);
-// }
+    default:
+        console.log('No Medal')
+        break;
+}
+// ---------------------------------------------------------- LOOP -------------------------------------------
+var i=1;
+for(i=1;i<4;i++){
+    console.log(i);
+}
 
-// for(var j=1;j<4;j++){
-//     console.log(j);
-// }
-// var counter=3;
-// while(counter>0){
-//     console.log(counter);
-//     counter = counter-1;
-// }
-// var counter1=3;
-// while(counter1>0){
-//     counter1 = counter1-1;
-//     console.log(counter1);
-// }
-// //---------------------------------------------------------------
-// for(var i=0; i<2;i++){
-//     for(var j=1; j<=7; j++){
-//         console.log("week" + i + "day" +j);
-//     }
-// }
+for(var j=1;j<4;j++){
+    console.log(j);
+}
+var counter=3;
+while(counter>0){
+    console.log(counter);
+    counter = counter-1;
+}
+var counter1=3;
+while(counter1>0){
+    counter1 = counter1-1;
+    console.log(counter1);
+}
+//---------------------------------------------------------------
+for(var i=0; i<2;i++){
+    for(var j=1; j<=7; j++){
+        console.log("week" + i + "day" +j);
+    }
+}
 // ----------------------------------------FUNCTION-----------------------------------------
 function addTwoNums() {
     var a = 10;
