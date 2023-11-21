36b72329e866c799a0a7ffd7a69fb2847d950a1e
Umar Saeed <umar23lhr@gmail.com>
2023-11-21 09:02:02 +0500

    obj js

--- 21-11-23/obj.js
+++ 21-11-23/obj.js
@@ -1,6 +1,15 @@
-Math.random;
-var decimal = Math.random();
-console.log(decimal); 
-console.log(decimal * 10);
-var round = Math.trunc(decimal*10);
-console.log(round); 
+// Math.random;
+// var decimal = Math.random();
+// console.log(decimal); 
+// console.log(decimal * 10);
+// var round = Math.trunc(decimal*10);
+// console.log(round); 
+// ----------------------------------------------------
+var string1 = "la-la-la";
+console.log(string1.indexOf('l'));
+console.log(string1.indexOf('a'));
+console.log(string1.lastIndexOf('-')); 
+console.log(string1.split('-')); // Split String
+console.log(string1.toUpperCase());
+console.log(string1.toLowerCase());
+// ---------------------------------------------------------
